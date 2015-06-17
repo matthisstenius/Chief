7d660fa37721b4e23bc68145855bb71a0bfb9274
Matthis Stenius <matthis@me.com>
2015-06-17 13:13:15 +0200

    laravel 4 support

--- src/CommandBus.php
+++ src/CommandBus.php
@@ -1,6 +1,6 @@
 <?php namespace Matthis\Chief;
 
-use Illuminate\Contracts\Container\Container;
+use Illuminate\Container;
 use Matthis\Chief\Exceptions\HandlerNotRegisteredException;
 use Matthis\Chief\Exceptions\InvalidCommandException;
 

e7f940d54679a6c5a9f4c69b29207cace4c41106
Matthis Stenius <matthis@me.com>
2015-12-11 17:06:02 +0100

    added test

--- spec/CommandBusSpec.php
+++ spec/CommandBusSpec.php
@@ -7,15 +7,19 @@ use spec\Matthis\Chief\TestDummies\AnotherCommand;
 use spec\Matthis\Chief\TestDummies\Invalid;
 use spec\Matthis\Chief\TestDummies\TestCommand;
 use spec\Matthis\Chief\TestDummies\Handlers\TestCommandHandler;
+use Illuminate\Contracts\Queue\Queue;
 
 class CommandBusSpec extends ObjectBehavior
 {
+    function let(Container $container, Queue $queue)
+    {
+        $this->beConstructedWith($container, $queue);
+    }
+
     function it_should_execute_a_command_to_command_handler(Container $container)
     {
         $container->make('spec\Matthis\Chief\TestDummies\Handlers\TestCommandHandler')
             ->willReturn(new TestCommandHandler);
-
-        $this->beConstructedWith($container);
 
         $this->execute(new TestCommand)->shouldReturn('Command handled successfully');
     }
@@ -23,7 +27,6 @@ class CommandBusSpec extends ObjectBehavior
     function it_should_fail_when_handler_class_does_not_exist(Container $container)
     {
         $container->make(AnotherCommand::class . 'Handler')->willReturn(null);
-        $this->beConstructedWith($container);
 
         $this->shouldThrow('Matthis\Chief\Exceptions\HandlerNotRegisteredException')
             ->during('execute', [new AnotherCommand]);
@@ -31,9 +34,17 @@ class CommandBusSpec extends ObjectBehavior
 
     function it_should_fail_when_command_is_named_incorrectly(Container $container)
     {
-        $this->beConstructedWith($container);
-
         $this->shouldThrow('Matthis\Chief\Exceptions\InvalidCommandException')
             ->during('execute', [new Invalid]);
+    }
+
+    function it_should_queue_a_command_for_later_execution(Container $container, Queue $queue)
+    {
+        $command = new TestCommand;
+
+        $container->make('spec\Matthis\Chief\TestDummies\Handlers\TestCommandHandler')
+            ->willReturn($command);
+
+        $queue->push('Matthis\Chief\CommandBus@execute', serialize($command));
     }
 }
